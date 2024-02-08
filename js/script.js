f2103280ea2cb0297d3b2a7d1949e0a997fd513e
Peter-Trebaticky <ptrebatickyp@gmail.com>
2024-02-08 10:27:46 +0100

    edit

--- js/script.js
+++ js/script.js
@@ -1,77 +1,64 @@
-(function () {
+(() => {
   "use strict";
 
-  let productsEl = document.querySelector(".products");
+  const productsEl = document.querySelector(".products");
 
-  fetch("https://dummyjson.com/products")
-    .then(response => response.json())
-    .then(data => {
-      generateProductList(data.products);
-
-      //Add to Cart
-      let addToCartButtons = document.querySelectorAll(".add-to-cart");
-
-      addToCartButtons.forEach(function (button) {
-        button.addEventListener("click", function (event) {
-          let productId = event.currentTarget.getAttribute("data-id");
-          let product = data.products.find(item => item.id === parseInt(productId));
-          console.log("Full Product Information:", product);
-        });
-      });
-    })
-    .catch(error => console.error("API Error:", error));
-
-
-    //template
-  let generateProductList = function (products) {
-    products.forEach(function (item) {
-      let productEl = document.createElement("div");
+  try {
+    const response = fetch("https://dummyjson.com/products");
+    const { products } = response.json();
+    
+    generateProductList(products);
+  } catch (error) {
+    console.error("API Error:", error);
+  }
+  //template
+  function generateProductList(products) {
+    products.forEach(item => {
+      const { thumbnail, brand, title, stock, price, discountPercentage, description, rating, id } = item;
+      const productEl = document.createElement("div");
       productEl.className = "product";
-      productEl.innerHTML = `<div class="product-image">
-                                <img src="${item.thumbnail}" alt="${item.title}">
-                             </div>
-                             <div class="product-brand">${item.brand}</div>
-                             <div class="product-name">${item.title}</div>
-                             <div class="product-stock ${item.stock > 0 ? 'in-stock' : 'out-of-stock'}"> ${item.stock > 0 ? "Product In Stock" : "Product Out of Stock"}</div>
-                             <div class="product-price">${item.discountPercentage < 5 ? `${item.price} $` : ''}</div>
-                             <div class="product-discountPercentage"> ${item.discountPercentage > 5 ? `${item.discountPercentage} %` : ''}</div>
-                             <div class="product-price-discount"> ${item.discountPercentage > 5 ? calculateDiscountedPrice(item.price, item.discountPercentage) + ' $' : ''}</div>
-                             <div class="product-description">${item.description}</div>
-                             <div class="product-rating"><span>Rating:</span> ${item.rating}</div>
-                             <div class="product-add-to-cart">
-                               <a href="#0" class="button see-more" data-id="${item.id}">More Details</a>
-                               <a href="#0" class="button add-to-cart" data-id="${item.id}">Add to Cart</a>
-                             </div>
-                          </div>`;
+      productEl.innerHTML = `
+        <div class="product-image">
+          <img src="${thumbnail}" alt="${title}">
+        </div>
+        <div class="product-brand">${brand}</div>
+        <div class="product-name">${title}</div>
+        <div class="product-stock ${stock > 0 ? 'in-stock' : 'out-of-stock'}">${stock > 0 ? "Product In Stock" : "Product Out of Stock"}</div>
+        ${discountPercentage < 5 ? `<div class="product-price">${price} $</div>` : ''}
+        ${discountPercentage > 5 ? `<div class="product-discountPercentage">${discountPercentage} %</div>` : ''}
+        ${discountPercentage > 5 ? `<div class="product-price-discount">${calculateDiscountedPrice(price, discountPercentage)} $</div>` : ''}
+        <div class="product-description">${description}</div>
+        <div class="product-rating"><span>Rating:</span> ${rating}</div>
+        <div class="product-add-to-cart">
+          <a href="#0" class="button see-more" data-id="${id}">More Details</a>
+          <a href="#0" class="button add-to-cart" data-id="${id}">Add to Cart</a>
+        </div>
+      `;
 
       productsEl.appendChild(productEl);
-          
+    });
+    //see-more
+    document.querySelectorAll(".see-more").forEach(button => {
+      button.addEventListener("click", ({ target }) => {
+        const productDiv = target.closest(".product");
+        const productRating = productDiv.querySelector(".product-rating");
+        const productDescription = productDiv.querySelector(".product-description");
 
-      //see-more
-      let seeMoreButtons = document.querySelectorAll(".see-more");
-      seeMoreButtons.forEach(function (button) {
-        button.addEventListener("click", function (event) {
-          let productDiv = event.currentTarget.closest(".product");
-          let productRating = productDiv.querySelector(".product-rating");
-          let productDescription = productDiv.querySelector(".product-description");
+        productRating.classList.add("product-show");
+        productDescription.classList.add("product-show");
 
-          productRating.classList.add("product-show");
-          productDescription.classList.add("product-show");
-
-          setTimeout(function () {
-            productRating.classList.remove("product-show");
-            productDescription.classList.remove("product-show");
-          }, 5000);
-        });
+        setTimeout(() => {
+          productRating.classList.remove("product-show");
+          productDescription.classList.remove("product-show");
+        }, 5000);
       });
-  });
+    });
+  }
 
-  //count prize  
-  };
+  //count price 
   function calculateDiscountedPrice(originalPrice, discountPercentage) {
     const discountAmount = (discountPercentage / 100) * originalPrice;
     const discountedPrice = originalPrice - discountAmount;
     return `<del>${originalPrice.toFixed(2)} $</del> ${discountedPrice.toFixed(2)} `;
   }
-
 })();
